a90e3162d3343e718658a6935e5a7a72b6e5fafe
Oksana Dolzhko <oksana.dolzhko@gmail.com>
2021-05-13 14:45:44 +0300

    ./rozmitka_pages

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+	$("#owl-example").owlCarousel({
+		navigation: true,
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		singleItem: true,
+		pagination: false,
+		rewindSpeed: 500
+	});
+});
