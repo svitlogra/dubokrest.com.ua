4f433f34758f7194aea994eca2c1bf4bc67e0210
Oksana Dolzhko <oksana.dolzhko@gmail.com>
2021-08-12 18:06:21 +0300

    12.08

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,6 @@
 console.log('file 1');
 
-AOS.init();
 
-// You can also pass an optional settings object
-// below listed default settings
 AOS.init({
   // Global settings:
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
@@ -17,12 +14,73 @@ AOS.init({
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
+  offset: 100, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400, // values from 0 to 3000, with step 50ms
+  duration: 500, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
   anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
 });
+
+//Swiper
+import { Swiper, Parallax, Mousewheel, Pagination } from 'swiper'
+  // import Swiper styles
+Swiper.use([ Parallax, Mousewheel, Pagination ])
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const swiperHotel = new Swiper('.hotel-room__slider', {
+  
+  speed:1500,  
+  parallax:true,
+  // direction: 'vertical',
+  // loop: true,
+  
+  // grabCursor: true,
+  
+  mousewheel: {
+    invert: false,
+    
+    },
+  
+  pagination: {
+    el: '.swiper-pagination',
+      type: 'bullets',
+      // clickable: true,
+    dynamicBullets: true,
+    },
+    
+      
+    
+    
+  });
+  
+  const swiperRestrount = new Swiper ('.restrount__slider', {
+    speed:1500,  
+    parallax:true,
+    grabCursor: true,
+    // direction: 'vertical',
+    // loop: true,
+   
+    mousewheel: {
+      invert: false,
+      
+      },
+    
+    pagination: {
+      el: '.swiper-pagination',
+        type: 'bullets',
+        // clickable: true,
+      dynamicBullets: true,
+      },
+      
+    
+  })
+
+});
+
+ 
+  
+  
